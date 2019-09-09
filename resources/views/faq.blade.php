055ca7ef578e9d1d07262edb062bfa8076f7c31c
natalia <natalia.poggi@gmail>
2019-09-09 14:27:37 -0300

    agrego margenes a faq ( igual le falta mucho a esa parte

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -5,8 +5,7 @@
 @endsection
 
 @section('content')
-
-        <div class="ContactContainerForm text-center">
+        <div class="ContactContainerForm text-center" style="margin-left:1%;margin-right:1%">
           <div class="ContactTopText">
             <h3>
               <!--a class="anchor" name="general-questions" href="#general-questions"></a-->
@@ -61,5 +60,4 @@
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
         </div>
-      
 @endsection
